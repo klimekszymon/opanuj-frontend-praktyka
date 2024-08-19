c26639c10b29922119abd22a17c893d83dabceee
klimekszymon <154986331+klimekszymon@users.noreply.github.com>
2024-08-19 22:00:51 +0200

    fixed navigation

--- practice/vue-app/src/router/index.ts
+++ practice/vue-app/src/router/index.ts
@@ -2,21 +2,25 @@ import { createRouter, createWebHistory } from "vue-router";
 import Index from "../views/index.vue";
 
 const views = import.meta.glob("@/views/*.vue");
-const exampleViews = import.meta.glob("@/views/examples/*.vue");
-const exerciseViews = import.meta.glob("@/views/exercises/*.vue");
-const liveQuizViews = import.meta.glob("@/views/live-quiz/*.vue");
-const challengeViews = import.meta.glob("@/views/challenges/*.vue");
-const takeHomeExamViews = import.meta.glob("@/views/take-home-exam/*.vue");
+const lessonViews = import.meta.glob("@/views/lessons/*.vue");
+const countriesViews = import.meta.glob("@/views/countries/*.vue");
+// const exampleViews = import.meta.glob("@/views/examples/*.vue");
+// const exerciseViews = import.meta.glob("@/views/exercises/*.vue");
+// const liveQuizViews = import.meta.glob("@/views/live-quiz/*.vue");
+// const challengeViews = import.meta.glob("@/views/challenges/*.vue");
+// const takeHomeExamViews = import.meta.glob("@/views/take-home-exam/*.vue");
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     ...generateRoutes(views),
-    ...generateRoutes(exampleViews, "/examples"),
-    ...generateRoutes(exerciseViews, "/exercises"),
-    ...generateRoutes(liveQuizViews, "/live-quiz"),
-    ...generateRoutes(challengeViews, "/challenges"),
-    ...generateRoutes(takeHomeExamViews, "/take-home-exam"),
+    ...generateRoutes(lessonViews, "/lessons"),
+    ...generateRoutes(countriesViews, "/countries"),
+    // ...generateRoutes(exampleViews, "/examples"),
+    // ...generateRoutes(exerciseViews, "/exercises"),
+    // ...generateRoutes(liveQuizViews, "/live-quiz"),
+    // ...generateRoutes(challengeViews, "/challenges"),
+    // ...generateRoutes(takeHomeExamViews, "/take-home-exam"),
   ],
 });
 
